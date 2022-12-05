b4e13ad5b4a884ac266ddd6ea17344e767f4da6f
Lugovtsov Gleb <lugovtsov.gs@phystech.edu>
2022-12-05 13:29:26 +0300

    mv project to lab-helper

--- project/materials/u02/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gcc gds.c -o gds -g
--- project/materials/u02/shorten.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#works only for ascii files with headers
-#prints every 100th line
-cat $1 | sed '1,/Waveform/d' | awk '!(NR%100)||(NR==2)'> $2
